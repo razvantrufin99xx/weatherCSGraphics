ed0354f9044f5e9ebd61072a78ca6f528382d0f1
Răzvan Trufin <144681144+razvantrufin99xx@users.noreply.github.com>
2024-05-31 14:36:27 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 5/31/2024
+ * Time: 1:33 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace weatherGraphics
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Panel panel1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(13, 48);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(846, 48);
+			this.textBox1.TabIndex = 1;
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(13, 102);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(838, 636);
+			this.panel1.TabIndex = 2;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1030, 855);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "weatherGraphics";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,127 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 5/31/2024
+ * Time: 1:33 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace weatherGraphics
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			Left = 0;
+			Top = 0;
+			f = Font;
+			g = panel1.CreateGraphics();
+		}
+		public float halfa = 0.0f;
+		public float halfb = 0.0f;
+		public void drawYear(ref float [] an, float hf, ref Pen p, ref Brush b, int ypost, int h)
+		{
+			//doar pentru date pozitive toate
+			//int h = 300;
+			float px = 1;
+			float py = h;
+			float ste = 50;
+			float x = (1 + 0) * ste;
+			float y = h-(3*an[0]- hf);
+			g.DrawLine(p, 0, h, 800, h);
+			string s;
+			for (int i = 0; i < 12; i++) {
+				
+				 x = (1 + i) * ste;
+				 y = h-(3*an[i]- hf);
+				 s = an[i].ToString();
+				 if(s.Length>=5){s = s.Substring(0,5);}
+				 g.DrawString(s,f,b,x+5,ypost);
+				g.DrawLine(p, x, y, px, py);
+				g.DrawLine(p, x, 0, x, 600);
+				px = x;
+				py = y;
+			}
+		}
+		public void caldifsani(ref float [] an1, ref float [] an2, ref float [] dif)
+		{
+			
+			for(int i = 0 ; i < 12 ; i++)
+			{
+				dif[i] = an1[i]-an2[i];
+			}
+		}
+		public float findmin(ref float [] a)
+		{
+			float min = a[0];
+			for(int i = 1 ; i < 12 ; i++)
+			{
+				if(a[i]<a[0]){min=a[i];}
+			}
+			return min;
+		}
+		public float findmax(ref float [] a)
+		{
+			float max = a[0];
+			for(int i = 1 ; i < 12 ; i++)
+			{
+				if(a[i]>a[0]){max=a[i];}
+			}
+			return max;
+		}
+		public float findHalfMinMax(float a, float b)
+		{
+			return Math.Abs(a-b)/2;
+		}
+		public string [] month = {"JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"};
+		public float [] an1874 = {19.0f,18.9f,23.3f,29.6f,51.3f,58.1f,65.3f,64.4f,60.0f,45.7f,29.9f,21.0f};
+		public float [] an1875 = {5.9f,1.3f,19.4f,33.3f,48.5f,56.7f,63.0f,61.5f,52.8f,39.9f,28.5f,25.7f};
+		public float [] difani  = new float[12];
+		
+		public Graphics g;
+		public Pen pa = new Pen(Color.FromArgb(200,156,57,59),3);
+		public Pen pb = new Pen(Color.FromArgb(200,255,100,59),3);
+		public Pen pd = new Pen(Color.FromArgb(200,0,57,100),3);
+		public Brush ba = new SolidBrush(Color.Blue);
+		public Brush bb = new SolidBrush(Color.Green);
+		public Brush bd = new SolidBrush(Color.Red);
+		
+		public Font f;
+		void Button1Click(object sender, EventArgs e)
+		{
+			float maxa = findmax(ref an1874);
+			float mina = findmin(ref an1874);
+			 halfa  = findHalfMinMax(maxa, mina);
+			 
+			 float maxb = findmax(ref an1875);
+			float minb = findmin(ref an1875);
+			 halfb  = findHalfMinMax(maxb, minb);
+			
+			caldifsani(ref an1874, ref an1875, ref difani);
+				
+			drawYear(ref an1874,halfa,ref pa,ref ba,10,300);
+			drawYear(ref an1875,halfb, ref pb,ref bb,30,300);
+			drawYear(ref difani,halfa,ref pd, ref bd,60,500);
+		}
+		
+	}
+}
