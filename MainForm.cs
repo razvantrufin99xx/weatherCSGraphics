e6901f44aad91e7139d389c96d53b328dc864b58
Răzvan Trufin <144681144+razvantrufin99xx@users.noreply.github.com>
2024-05-31 21:49:22 +0300

    Update MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -65,8 +65,8 @@ namespace weatherGraphics
 				
 				 g.DrawString(month[i],f,b,x+5,600-30);
 				 g.DrawLine(p, x, y-correction, px, py-correction);
-				
-				g.DrawLine(p, x, 0, x, 600);
+				 g.DrawEllipse(ph2, x-2, y-correction-2, 4,4);
+				g.DrawLine(ph, x, 0, x, 600);
 				g.DrawLine(ph, 0, y, 600, y);
 				
 				g.DrawString(s,f,b,x,y-35);
@@ -110,10 +110,11 @@ namespace weatherGraphics
 		public float [] difani  = new float[12];
 		
 		public Graphics g;
-		public Pen pa = new Pen(Color.FromArgb(200,156,57,59),3);
-		public Pen pb = new Pen(Color.FromArgb(200,255,100,59),3);
-		public Pen pd = new Pen(Color.FromArgb(200,0,57,100),3);
+		public Pen pa = new Pen(Color.FromArgb(200,156,57,59),1);
+		public Pen pb = new Pen(Color.FromArgb(200,255,100,59),1);
+		public Pen pd = new Pen(Color.FromArgb(200,0,57,100),1);
 		public Pen ph = new Pen(Color.FromArgb(20,0,57,100),1);
+		public Pen ph2 = new Pen(Color.FromArgb(100,0,57,100),1);
 		public Brush ba = new SolidBrush(Color.Blue);
 		public Brush bb = new SolidBrush(Color.Green);
 		public Brush bd = new SolidBrush(Color.Red);
@@ -132,7 +133,7 @@ namespace weatherGraphics
 			caldifsani(ref an1874, ref an1875, ref difani);
 				
 			drawYear(ref an1874,halfa,ref pa,ref ba,10,300,"1874");
-			drawYear(ref an1875,halfb, ref pb,ref bb,30,300,"1875");
+			drawYear(ref an1875,halfb, ref pb,ref bb,25,300,"1875");
 			drawYear(ref difani,halfa,ref pd, ref bd,40,500,"1875-1874");
 		}
 		
